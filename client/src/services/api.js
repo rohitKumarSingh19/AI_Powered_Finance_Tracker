873a67ba1714bd61e7328e873b7a1d0317aa8ebb
rohitsingh <rohit.k.chauhan1997@gmail.com>
2025-09-18 22:59:34 +0530

    add backend deploy url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL:"https://ai-powered-finance-tracker-1-b50f.onrender.com/",
+  //baseURL: "http://localhost:5000/api",
+  baseURL:"https://ai-powered-finance-tracker-1-b50f.onrender.com/",
 });
 
 export default API;
